0043e54f965000e49ab42683b5a0c66448351fa9
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-10-21 21:47:30 +0900

    :recycle: Auth persistence -> table 별 persistence로 분리

--- src/main/java/com/promptoven/authservice/adaptor/infrastructure/jpa/MemberPersistenceWithJpa.java
+++ src/main/java/com/promptoven/authservice/adaptor/infrastructure/jpa/MemberPersistenceWithJpa.java
@@ -1,4 +1,67 @@
 package com.promptoven.authservice.adaptor.infrastructure.jpa;
 
-public class MemberPersistenceWithJpa {
+import org.springframework.stereotype.Service;
+
+import com.promptoven.authservice.adaptor.infrastructure.jpa.entity.MemberEntity;
+import com.promptoven.authservice.adaptor.infrastructure.jpa.repository.MemberRepository;
+import com.promptoven.authservice.application.port.out.call.MemberPersistence;
+import com.promptoven.authservice.domain.Member;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class MemberPersistenceWithJpa implements MemberPersistence {
+
+	private final MemberRepository memberRepository;
+
+	@Override
+	public void create(Member member) {
+		MemberEntity memberEntity = MemberEntity.fromDomain(member);
+		memberRepository.save(memberEntity);
+	}
+
+	@Override
+	public Member findByEmail(String email) {
+		MemberEntity memberEntity = memberRepository.findByEmail(email);
+		if (memberEntity == null) {
+			return null;
+		}
+		return memberEntity.toDomain();
+	}
+
+	@Override
+	public Member findByUuid(String uuid) {
+		MemberEntity memberEntity = memberRepository.findByUuid(uuid);
+		if (memberEntity == null) {
+			return null;
+		}
+		return memberEntity.toDomain();
+	}
+
+	@Override
+	public boolean existsByEmail(String email) {
+		return memberRepository.existsByEmail(email);
+	}
+
+	@Override
+	public boolean existsByNickname(String nickname) {
+		return memberRepository.existsByNickname(nickname);
+	}
+
+	@Override
+	public void updatePassword(Member updatedMember) {
+		MemberEntity memberEntity = MemberEntity.fromDomain(updatedMember);
+		memberEntity.setId(memberRepository.findByUuid(updatedMember.getUuid()).getId());
+		memberRepository.save(memberEntity);
+	}
+
+	@Override
+	public void remove(Member member) {
+		MemberEntity memberEntity = MemberEntity.fromDomain(member);
+		memberEntity.setId(memberRepository.findByUuid(member.getUuid()).getId());
+		memberRepository.save(memberEntity);
+	}
 }
--- src/main/java/com/promptoven/authservice/adaptor/infrastructure/jpa/RolePersistenceWithJpa.java
+++ src/main/java/com/promptoven/authservice/adaptor/infrastructure/jpa/RolePersistenceWithJpa.java
@@ -1,4 +1,31 @@
 package com.promptoven.authservice.adaptor.infrastructure.jpa;
 
-public class RolePersistenceWithJpa {
+import org.springframework.stereotype.Service;
+
+import com.promptoven.authservice.adaptor.infrastructure.jpa.entity.RoleEntity;
+import com.promptoven.authservice.adaptor.infrastructure.jpa.repository.RoleRepository;
+import com.promptoven.authservice.application.port.out.call.RolePersistence;
+import com.promptoven.authservice.domain.Role;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class RolePersistenceWithJpa implements RolePersistence {
+
+	private final RoleRepository roleRepository;
+
+	@Override
+	public void create(Role role) {
+		RoleEntity roleEntity = RoleEntity.fromDomain(role);
+		roleRepository.save(roleEntity);
+	}
+
+	@Override
+	public Role findRoleById(int roleID) {
+		RoleEntity roleEntity = roleRepository.findById(roleID).orElse(new RoleEntity());
+		return roleEntity.toDomain();
+	}
 }
