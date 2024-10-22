2f9aa6736e5a53f46c62338a2ea53d03800b1d38
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-10-22 13:58:31 +0900

    :recycle: 닉네임 변경 컨트롤러 메소드에 VO 처리

--- src/main/java/com/promptoven/authservice/adaptor/web/controller/MemberRestController.java
+++ src/main/java/com/promptoven/authservice/adaptor/web/controller/MemberRestController.java
@@ -1,9 +1,11 @@
 package com.promptoven.authservice.adaptor.web.controller;
 
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.promptoven.authservice.adaptor.web.controller.vo.in.UpdateNicknameRequestVO;
 import com.promptoven.authservice.application.port.in.usecase.MemberUseCases;
 
 import lombok.RequiredArgsConstructor;
@@ -18,7 +20,7 @@ public class MemberRestController {
 	private final MemberUseCases memberUseCases;
 
 	@PutMapping("/nickname")
-	public void updateNickname(String memberUUID, String nickname) {
-		memberUseCases.updateNickname(memberUUID, nickname);
+	public void updateNickname(@RequestBody UpdateNicknameRequestVO updateNicknameRequestVO) {
+		memberUseCases.updateNickname(updateNicknameRequestVO.getMemberUUID(), updateNicknameRequestVO.getNickname());
 	}
 }
