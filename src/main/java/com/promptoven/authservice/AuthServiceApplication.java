15b8a8579fd7f82ca86dd4211f3b47df57d74972
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-10-23 10:06:14 +0900

    Bug :bug: Eureka Client 관련 Application Annotation 추가

--- src/main/java/com/promptoven/authservice/AuthServiceApplication.java
+++ src/main/java/com/promptoven/authservice/AuthServiceApplication.java
@@ -2,16 +2,18 @@ package com.promptoven.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy 
+@EnableDiscoveryClient
+@EnableAspectJAutoProxy
 @EnableTransactionManagement
 public class AuthServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AuthServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
 
 }
