b84c843349e26a96eb21516e00fe6b6b6580b635
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-10-31 22:41:05 +0900

    :recycle: Refactor - member find by UUID를 service method에서 annotation+aspect를 통해 처리하도록 수정

--- src/main/java/com/promptoven/authservice/adaptor/web/controller/MemberAdminRestController.java
+++ src/main/java/com/promptoven/authservice/adaptor/web/controller/MemberAdminRestController.java
@@ -1,12 +1,27 @@
 package com.promptoven.authservice.adaptor.web.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.promptoven.authservice.adaptor.web.controller.vo.in.BanRequestVO;
+import com.promptoven.authservice.adaptor.web.controller.vo.in.ClearPasswordRequestVO;
+import com.promptoven.authservice.adaptor.web.controller.vo.in.CreateRoleRequestVO;
+import com.promptoven.authservice.adaptor.web.controller.vo.in.DeleteRoleRequestVO;
+import com.promptoven.authservice.adaptor.web.controller.vo.in.RegisterRequestVO;
+import com.promptoven.authservice.adaptor.web.controller.vo.in.SetMemberRoleRequestVO;
+import com.promptoven.authservice.adaptor.web.controller.vo.in.UnbanRequestVO;
+import com.promptoven.authservice.adaptor.web.controller.vo.in.UpdateNicknameRequestVO;
+import com.promptoven.authservice.adaptor.web.controller.vo.in.UpdateRoleRequestVO;
 import com.promptoven.authservice.application.port.in.usecase.MemberManagementUseCase;
 import com.promptoven.authservice.application.port.in.usecase.MemberRegistrationUseCase;
-import com.promptoven.authservice.adaptor.web.controller.vo.in.*;
 import com.promptoven.authservice.application.port.in.usecase.RoleManagementUseCase;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -26,29 +41,29 @@ public class MemberAdminRestController {
 
 	@PutMapping("/ban")
 	public void banMember(@RequestBody BanRequestVO banRequestVO) {
-		memberManagementUseCases.banMember(banRequestVO.getMemberUUID());
+		memberManagementUseCases.banMember(banRequestVO.getMemberUUID(), null);
 	}
 
 	@PutMapping("/unban")
 	public void unbanMember(@RequestBody UnbanRequestVO unbanRequestVO) {
-		memberManagementUseCases.unbanMember(unbanRequestVO.getMemberUUID());
+		memberManagementUseCases.unbanMember(unbanRequestVO.getMemberUUID(), null);
 	}
 
 	@PutMapping("/nickname")
 	public void updateNickname(@RequestBody UpdateNicknameRequestVO updateNicknameRequestVO) {
 		memberManagementUseCases.updateNickname(
-			updateNicknameRequestVO.getMemberUUID(), updateNicknameRequestVO.getNickname());
+			updateNicknameRequestVO.getMemberUUID(), updateNicknameRequestVO.getNickname(), null);
 	}
 
 	@PutMapping("/member-role")
 	public void updateMemberRole(@RequestBody SetMemberRoleRequestVO setMemberRoleRequestVO) {
 		memberManagementUseCases.setMemberRole(
-			setMemberRoleRequestVO.getMemberNickname(), setMemberRoleRequestVO.getRoleName());
+			setMemberRoleRequestVO.getMemberNickname(), setMemberRoleRequestVO.getRoleName(), null);
 	}
 
 	@PutMapping("/clearPW")
 	public void clearPassword(@RequestBody ClearPasswordRequestVO clearPasswordRequestVO) {
-		memberManagementUseCases.clearPassword(clearPasswordRequestVO.getMemberUUID());
+		memberManagementUseCases.clearPassword(clearPasswordRequestVO.getMemberUUID(), null);
 	}
 
 	@PostMapping("/role")
