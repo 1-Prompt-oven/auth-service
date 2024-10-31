4a02dbe93e195d76e2da4ccf07884fb6eeaa167b
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-10-31 22:41:05 +0900

    :recycle: Refactor - member find by UUID를 service method에서 annotation+aspect를 통해 처리하도록 수정

--- src/main/java/com/promptoven/authservice/application/service/MemberManagementService.java
+++ src/main/java/com/promptoven/authservice/application/service/MemberManagementService.java
@@ -1,59 +1,64 @@
 package com.promptoven.authservice.application.service;
 
+import org.springframework.stereotype.Service;
+
 import com.promptoven.authservice.application.port.in.usecase.MemberManagementUseCase;
 import com.promptoven.authservice.application.port.out.call.MemberPersistence;
 import com.promptoven.authservice.application.port.out.call.RolePersistence;
 import com.promptoven.authservice.application.service.annotation.FindMemberOperation;
 import com.promptoven.authservice.domain.Member;
 import com.promptoven.authservice.domain.Role;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberManagementService implements MemberManagementUseCase {
 
-    private final MemberPersistence memberPersistence;
-    private final RolePersistence rolePersistence;
+	private final MemberPersistence memberPersistence;
+	private final RolePersistence rolePersistence;
 
+	@Override
+	@FindMemberOperation
+	public void promoteToSeller(String memberUUID, Member member) {
+		memberPersistence.updateMember(Member.updateMemberRole(member, 2));
+	}
 
-    @Override
-    @FindMemberOperation
-    public void promoteToSeller(String memberUUID) {
-        memberPersistence.updateMember(Member.updateMemberRole(member, 2));
-    }
+	@Override
+	@FindMemberOperation
+	public void setMemberRole(String memberUUID, String roleName, Member member) {
+		// The aspect will convert the UUID to a Member object
+		Role role = rolePersistence.findByName(roleName);
+		memberPersistence.updateMember(Member.updateMemberRole(member, role.getId()));
+	}
 
-    @Override
-    public void setMemberRole(String memberUUID, String roleName) {
-        Role role = rolePersistence.findByName(roleName);
-        Member member = memberPersistence.findByUuid(memberUUID);
-        memberPersistence.updateMember(Member.updateMemberRole(member, role.getId()));
-    }
+	@Override
+	@FindMemberOperation
+	public void banMember(String memberUUID, Member member) {
+		// The aspect will convert the UUID to a Member object
+		memberPersistence.updateMember(Member.banMember(member));
+	}
 
-    @Override
-    public void banMember(String memberUUID) {
-        Member member = memberPersistence.findByUuid(memberUUID);
-        memberPersistence.updateMember(Member.banMember(member));
-    }
+	@Override
+	@FindMemberOperation
+	public void unbanMember(String memberUUID, Member member) {
+		// The aspect will convert the UUID to a Member object
+		memberPersistence.updateMember(Member.unbanMember(member));
+	}
 
-    @Override
-    public void unbanMember(String memberUUID) {
-        Member member = memberPersistence.findByUuid(memberUUID);
-        memberPersistence.updateMember(Member.unbanMember(member));
-    }
+	@Override
+	@FindMemberOperation
+	public void updateNickname(String memberUUID, String nickname, Member member) {
+		// The aspect will convert the UUID to a Member object
+		memberPersistence.updateMember(Member.updateMemberNickname(member, nickname));
+	}
 
-    @Override
-    public void updateNickname(String memberUUID, String nickname) {
-        Member member = memberPersistence.findByUuid(memberUUID);
-        memberPersistence.updateMember(Member.updateMemberNickname(member, nickname));
-    }
-
-    @Override
-    public void clearPassword(String memberUUID) {
-        Member member = memberPersistence.findByUuid(memberUUID);
-        memberPersistence.updateMember(Member.updateMemberPassword(member, "clear"));
-    }
-
+	@Override
+	@FindMemberOperation
+	public void clearPassword(String memberUUID, Member member) {
+		// The aspect will convert the UUID to a Member object
+		memberPersistence.updateMember(Member.updateMemberPassword(member, "clear"));
+	}
 }
