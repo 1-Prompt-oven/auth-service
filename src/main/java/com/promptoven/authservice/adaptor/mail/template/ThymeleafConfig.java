552751801a5c2714f5696cd8a27ff2e4bee57164
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-11-04 12:20:26 +0900

    :sparkles: Feature 이메일 본문이 html로 업그레이드 되었습니다. (타임리프 의존성 추가)

---
+++ src/main/java/com/promptoven/authservice/adaptor/mail/template/ThymeleafConfig.java
@@ -0,0 +1,28 @@
+package com.promptoven.authservice.adaptor.mail.template;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+} 
