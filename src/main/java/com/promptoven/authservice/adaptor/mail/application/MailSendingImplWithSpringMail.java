5d719f35b6268336322479b6f50fe0ddd0d15fd5
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-11-04 11:41:43 +0900

    :sparkles: Feature: Email body로 HTML 담을 수 있도록 update

--- src/main/java/com/promptoven/authservice/adaptor/mail/application/MailSendingImplWithSpringMail.java
+++ src/main/java/com/promptoven/authservice/adaptor/mail/application/MailSendingImplWithSpringMail.java
@@ -4,12 +4,15 @@ import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailSendException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.promptoven.authservice.application.port.out.call.MailSending;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class MailSendingImplWithSpringMail implements MailSending {
@@ -34,7 +37,7 @@ public class MailSendingImplWithSpringMail implements MailSending {
 		props.put("mail.transport.protocol", "smtp");
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		props.put("mail.smtp.ssl.trust", HOST);
 		props.put("mail.smtp.ssl.protocols", "TLSv1.3");
 		props.put("mail.debug", "true");
 
@@ -43,17 +46,19 @@ public class MailSendingImplWithSpringMail implements MailSending {
 
 	@Override
 	public void sendMail(String to, String subject, String text) {
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom(username);
-		message.setTo(to);
-		message.setSubject(subject);
-		message.setText(text);
+		JavaMailSender mailSender = getJavaMailSender();
+		MimeMessage message = mailSender.createMimeMessage();
 
 		try {
-			JavaMailSender mailSender = getJavaMailSender();
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+			helper.setFrom(username);
+			helper.setTo(to);
+			helper.setSubject(subject);
+			helper.setText(text, true); // true indicates HTML content
 			mailSender.send(message);
-		} catch (MailSendException e) {
-			throw e;
+
+		} catch (MessagingException e) {
+			throw new MailSendException("Failed to send email", e);
 		}
 	}
 }
