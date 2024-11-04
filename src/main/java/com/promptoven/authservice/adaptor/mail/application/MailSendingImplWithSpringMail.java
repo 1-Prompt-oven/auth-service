20aa4b62497a27bbf7c72121b5f556b14644bba9
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-11-04 12:20:26 +0900

    :sparkles: Feature 이메일 본문이 html로 업그레이드 되었습니다. (타임리프 의존성 추가)

--- src/main/java/com/promptoven/authservice/adaptor/mail/application/MailSendingImplWithSpringMail.java
+++ src/main/java/com/promptoven/authservice/adaptor/mail/application/MailSendingImplWithSpringMail.java
@@ -8,17 +8,23 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import com.promptoven.authservice.application.port.out.call.MailSending;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class MailSendingImplWithSpringMail implements MailSending {
 
 	private static final String HOST = "smtp.gmail.com";
 	private static final int PORT = 587;
+
+	private final SpringTemplateEngine templateEngine;
 
 	@Value("${spring.mail.username}")
 	private String username;
@@ -49,16 +55,24 @@ public class MailSendingImplWithSpringMail implements MailSending {
 		JavaMailSender mailSender = getJavaMailSender();
 		MimeMessage message = mailSender.createMimeMessage();
 
+		String mailBody = buildMailBody(text);
+
 		try {
 			MimeMessageHelper helper = new MimeMessageHelper(message, true);
 			helper.setFrom(username);
 			helper.setTo(to);
 			helper.setSubject(subject);
-			helper.setText(text, true); // true indicates HTML content
+			helper.setText(mailBody, true); // true indicates HTML content
 			mailSender.send(message);
 
 		} catch (MessagingException e) {
 			throw new MailSendException("Failed to send email", e);
 		}
+	}
+
+	private String buildMailBody(String text) {
+		Context context = new Context();
+		context.setVariable("inputedString", text);
+		return templateEngine.process("BaseMailBody", context);
 	}
 }
