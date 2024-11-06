56f9b2a3ff5c81ccf9b53e6085565d1c9c63ca84
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-11-06 16:04:17 +0900

    Kafka 기반 Event Pub&Sub 구현

--- src/main/java/com/promptoven/authservice/adaptor/infrastructure/kafka/KafkaConfig.java
+++ src/main/java/com/promptoven/authservice/adaptor/infrastructure/kafka/KafkaConfig.java
@@ -29,10 +29,9 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class KafkaConfig {
 
-	@Value("${kafka.bootstrap-servers}")
+	@Value("${spring.kafka.bootstrap-servers}")
 	private String bootstrapServers;
-
-	@Value("${kafka.consumer.group-id}")
+	@Value("${spring.kafka.consumer.group-id}")
 	private String groupId;
 
 	@Bean
@@ -45,8 +44,22 @@ public class KafkaConfig {
 	}
 
 	@Bean
+	public ProducerFactory<String, String> simpleProducerFactory() {
+		Map<String, Object> configProps = new HashMap<>();
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		return new DefaultKafkaProducerFactory<>(configProps);
+	}
+
+	@Bean
 	public KafkaTemplate<String, Object> kafkaTemplate() {
 		return new KafkaTemplate<>(producerFactory());
+	}
+
+	@Bean
+	public KafkaTemplate<String, String> simpleKafkaTemplate() {
+		return new KafkaTemplate<>(simpleProducerFactory());
 	}
 
 	@Bean
@@ -61,10 +74,29 @@ public class KafkaConfig {
 	}
 
 	@Bean
+	public ConsumerFactory<String, String> simpleConsumerFactory() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+		return new DefaultKafkaConsumerFactory<>(props);
+	}
+
+	@Bean
 	public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
 		ConcurrentKafkaListenerContainerFactory<String, Object> factory =
 			new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(consumerFactory());
+		return factory;
+	}
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, String> simpleKafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, String> factory =
+			new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(simpleConsumerFactory());
 		return factory;
 	}
 
