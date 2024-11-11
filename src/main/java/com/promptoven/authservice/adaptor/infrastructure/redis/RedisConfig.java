f87c9fc8996b266bfcc2b9670ed22b655b720f22
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-11-11 21:27:50 +0900

    Fix: build error (Import packages)

--- src/main/java/com/promptoven/authservice/adaptor/infrastructure/redis/RedisConfig.java
+++ src/main/java/com/promptoven/authservice/adaptor/infrastructure/redis/RedisConfig.java
@@ -1,13 +1,19 @@
 package com.promptoven.authservice.adaptor.infrastructure.redis;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.protocol.ProtocolVersion;
 
 @Configuration
 public class RedisConfig {
@@ -20,11 +26,10 @@ public class RedisConfig {
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		log.info("Creating Redis connection factory for {}:{}", redisHost, redisPort);
 
 		RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration();
-		serverConfig.setHostName(redisHost);
-		serverConfig.setPort(redisPort);
+		serverConfig.setHostName(host);
+		serverConfig.setPort(port);
 		serverConfig.setDatabase(0);
 
 		LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
