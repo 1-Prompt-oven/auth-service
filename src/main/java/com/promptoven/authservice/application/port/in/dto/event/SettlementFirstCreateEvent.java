bf30773ec9914bc0682151d34e1b12c1251f0286
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-11-25 16:40:11 +0900

    🐛 Fix Kafka Producer setting (remove type header)

--- src/main/java/com/promptoven/authservice/application/port/in/dto/event/SettlementFirstCreateEvent.java
+++ src/main/java/com/promptoven/authservice/application/port/in/dto/event/SettlementFirstCreateEvent.java
@@ -1,8 +1,14 @@
 package com.promptoven.authservice.application.port.in.dto.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class SettlementFirstCreateEvent {
 
 	private String memberUUID;
