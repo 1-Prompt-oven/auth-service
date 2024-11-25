adc3892cb284cba5e3aef04acb72b49ec7ad49b0
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-11-25 16:23:01 +0900

    🐛 Fix: UpdateNickName 관련 AOP 버그 수정

--- src/main/java/com/promptoven/authservice/application/port/in/dto/UpdateNicknameRequestDTO.java
+++ src/main/java/com/promptoven/authservice/application/port/in/dto/UpdateNicknameRequestDTO.java
@@ -1,18 +1,17 @@
 package com.promptoven.authservice.application.port.in.dto;
 
-import lombok.Builder;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @AllArgsConstructor
 @Getter
-public class UpdateNicknameRequestDTO implements MemberUUIDOnlyDTO {
-    private final String memberUUID;
-    private final String nickname;
-
-    @Override
-    public String memberUUID() {
-        return memberUUID;
-    }
+public class UpdateNicknameRequestDTO {
+	private final String memberUUID;
+	private final String nickname;
+    
+	public String memberUUID() {
+		return memberUUID;
+	}
 } 
